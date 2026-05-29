a92ae3182eb94abc389851afeced793f51d93725
PrinxeCharlex <2425403322@live.gctu.edu.gh>
2026-05-29 10:16:53 +0000

    Add files via upload

---
+++ pca.py
@@ -0,0 +1,38 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Prince Charles Akwentri
+# STUDENT ID: 2425403322
+# Email: 2425403322@live.gctu.edu.gh
+# Grader: Akwentri Prince Charles
+#
+# On my honor, Prince Charles Akwentri, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+def main():
+    # This program prints my initials in large letters
+
+    print()
+
+    print("...PCA")
+
+    print()
+
+    print("...PPPPPPPPPP....CCCCCCCCCC......AAAAAAAAAA........")
+    print("...PP........PP..CC..............AA......AA........")
+    print("...PP........PP..CC..............AA......AA........")
+    print("...PP........PP..CC..............AA......AA........")
+    print("...PPPPPPPPPP....CC..............AAAAAAAAAA........")
+    print("...PP............CC..............AA......AA........")
+    print("...PP............CC..............AA......AA........")
+    print("...PP............CC........... ..AA......AA........")
+    print("...PP......**....CCCCCCCCCC..**..AA......AA...**...")
+    print("...PP......**....CCCCCCCCCC..**..AA......AA...**...")
+
+    print()
+
+
+main()
